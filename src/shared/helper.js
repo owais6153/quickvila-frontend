f25007c0915028125066cacc900888e5fedb99eb
owais6153 <owais6153@gmail.com>
2022-10-27 12:41:11 -0700

    Production env

--- src/shared/helper.js
+++ src/shared/helper.js
@@ -1,12 +1,18 @@
 export const homeUrl = (path = "") => {
-  return `${process.env.REACT_APP_MY_APP_URL}${path}`;
+  if(process.env.REACT_APP_MY_APP_ENVIROMENT === 'local')
+    return `${process.env.REACT_APP_MY_APP_URL}${path}`;
+  else
+    return `${process.env.REACT_APP_MY_APP_URL_PRODUCTION}${path}`;
 };
 export const apiUrl = (path = "") => {
-  return `${process.env.REACT_APP_API_URL}${path}`;
+  if(process.env.REACT_APP_MY_APP_ENVIROMENT === 'local')
+    return `${process.env.REACT_APP_API_URL}${path}`;
+  else
+    return `${process.env.REACT_APP_API_URL_PRODUCTION}${path}`;
 };
 export const appName = () => {
   return `${process.env.REACT_APP_MY_APP}`;
 };
 export const Currency = () => {
-  return `${process.env.REACT_APP_CURRENCY}` || "$";
+  return `${process.env.REACT_APP_MY_APP_CURRENCY}` || "$";
 };
