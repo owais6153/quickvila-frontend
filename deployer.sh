0e7d68365413242b159e51c27cb9ef18e41ac372
owais6153 <owais6153@gmail.com>
2022-11-08 15:55:40 -0800

    Deploy test

--- deployer.sh
+++ deployer.sh
@@ -1,13 +1,9 @@
 set -e
 
-#Pull Code & Build.
-cd public_html/trikaro
+echo "Deployment Start"
 git pull origin master
 npm run build
-
 cd ../../
-
-#Copy the build files to public_html.
 rsync -a public_html/trikaro/build/* public_html/
-#Make sure .htaccess is set.
 cp public_html/trikaro/.htaccess public_html/.htaccess
+echo "Deployment Finished"
