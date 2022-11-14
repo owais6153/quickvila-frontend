ac425624e604e74da50003a7e39266baf2489c74
owais6153 <owais6153@gmail.com>
2022-11-14 14:51:23 +0500

    Deployer

--- deployer.sh
+++ deployer.sh
@@ -2,10 +2,8 @@ set -e
 
 echo "Deployment Start"
 git pull origin master
-echo "Build Start"
 npm run build
 cd ../../
-echo "Moving build to public"
 rsync -a public_html/trikaro/build/* public_html/
 cp public_html/trikaro/.htaccess public_html/.htaccess
 echo "Deployment Finished"
