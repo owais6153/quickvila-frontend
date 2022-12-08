bccce9da262d54cbe0a2548c13cc6488a5a53503
owais6153 <owais6153@gmail.com>
2022-12-08 17:01:31 +0500

    Error 404

--- src/shared/components/component-404.js
+++ src/shared/components/component-404.js
@@ -1,21 +1,11 @@
-import Container from "react-bootstrap/esm/Container";
-import { homeUrl } from "../helper";
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 
 const Component404 = () => {
   return (
-    <Container
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        textAlign: "center",
-      }}
-    >
+    <div id="error-boundry">
       <Helmet>
-        <title>404 - Page Not Found | QuiclVila</title>
+        <title>Opps! Page not found | QuiclVila</title>
         <meta
           name="description"
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -24,15 +14,14 @@ malesuada mauris. Orci tellus ut ornare varius sed massa
 quis vel."
         />
       </Helmet>
-      <img src={homeUrl("/images/404.webp")} alt="404 icon" className="" />
-      <h2>
-        Sorry! Page you are requesting <br />
-        is not found.
-      </h2>
-      <Link to="/" className="btn btn-primary">
-        Go to Home
-      </Link>
-    </Container>
+      <div className="catch">
+        <h1>Opps! Page not found</h1>
+        <p>The page you are looking for is not found on this url.</p>
+        <Link to="/" className="btn btn-primary">
+          Go to Home
+        </Link>
+      </div>
+    </div>
   );
 };
 export default Component404;
