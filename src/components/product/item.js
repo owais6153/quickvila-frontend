bec4dba74258d95acfdcaa01b7d0c4c96d57ca52
owais6153 <owais6153@gmail.com>
2022-12-12 16:34:43 +0500

    Link

--- src/components/product/item.js
+++ src/components/product/item.js
@@ -2,7 +2,7 @@ import { Currency } from "../../shared/helper";
 import { Link } from "react-router-dom";
 const ProductItem = ({ product }) => {
   return (
-    <Link to={`/stores/${product.store_id}/products/${product.id}`}>
+    <a href={`/stores/${product.store_id}/products/${product.id}`}>
       <div className="prd-cBox">
         <img src={product.image} alt={product.name} className="w-100" />
         <div className="ctn-p text-center">
@@ -21,7 +21,7 @@ const ProductItem = ({ product }) => {
           </div>
         </div>
       </div>
-    </Link>
+    </a>
   );
 };
 export default ProductItem;
