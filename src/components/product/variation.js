eb73d3e946fefae20b0953e4bcb1504238eb3fbd
owais6153 <owais6153@gmail.com>
2022-12-14 15:40:40 +0500

    Variation output

--- src/components/product/variation.js
+++ src/components/product/variation.js
@@ -1,13 +1,54 @@
-const Variations = ({ options, variations }) => {
+import { useState } from "react";
+
+const VariationInput = ({ item, labelname, changeHandler, className }) => {
   return (
-    <>
+    <label style={{ backgroundColor: item.media }} className={className}>
+      <input
+        onChange={changeHandler}
+        type="radio"
+        name={`variation[${labelname}]`}
+        value={item.name}
+        data-labelname={labelname}
+      />
+      {item.name}
+    </label>
+  );
+};
+
+const Variations = ({ options, variations }) => {
+  const [selected, setSelected] = useState([]);
+  const changeHandler = (e) => {
+    console.log(1);
+    let s = selected;
+    s[e.target.dataset.labelname] = e.target.value;
+    setSelected(s);
+  };
+  return (
+    <div id="variations">
       {Object.keys(options).map((key) => {
-        return options[key].map((i) => {
-          console.log(i.name);
-          return <div key={i.name}>{i.name}</div>;
-        });
+        return (
+          <>
+            <p>{key}</p>
+            <div
+              key={options[key][0].name}
+              className={options[key][0].media != null ? "media" : "custom"}
+            >
+              {options[key].map((i, index) => {
+                return (
+                  <VariationInput
+                    key={`${index}${i.name}`}
+                    item={i}
+                    labelname={key}
+                    changeHandler={changeHandler}
+                    className={selected[key] == i.name ? "active" : ""}
+                  />
+                );
+              })}
+            </div>
+          </>
+        );
       })}
-    </>
+    </div>
   );
 };
 export default Variations;
